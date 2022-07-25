4e425f20a95162554c07a81fb238098a644b58f6
赵少山 <zhaoshaoshan@qq.com>
2022-07-26 00:22:35 +0800

    all codes

---
+++ src/main/java/org/mydemo/SkywalkingTest.java
@@ -0,0 +1,8 @@
+package org.mydemo;
+
+public class SkywalkingTest {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("This is SkywalkingTest main method.");
+        Thread.sleep(10);
+    }
+}
